f286a6b84291d360a1b6d409437a1f679be4b912
kkresic <kristijan@agilno.com>
2023-04-02 08:10:27 +0200

    Setup form fields and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import TextField from "./components/TextField/TextField";
+import styles from './App.module.scss'
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import ageSchema from "./validations/ageSchema";
+import addLeadingZero from "./utils/addLeadingZero";
+import SubmitIcon from "./icons/SubmitIcon";
+
+interface Inputs {
+    day: string;
+    month: string;
+    year: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>({
+        mode: 'onBlur',
+        resolver: zodResolver(ageSchema)
+    });
 
-  return (
-    <h1>
-        Hello world
-    </h1>
-  )
+    return (
+        <div className={styles.app}>
+            <div className={styles.formWrapper}>
+                <div>
+                    <TextField id="day" label="Day" name="day" error={errors.day?.message}
+                               placeholder="DD"
+                               inputProps={register('day', {
+                                   onChange: (e) => {
+                                       e.target.value = e.target.value.replace(/[^0-9]/g, '')
+                                   },
+                                   onBlur: (e) => {
+                                       const day = parseInt(e.target.value, 10)
+                                       e.target.value = addLeadingZero(day)
+                                   }
+                               })}/>
+                </div>
+                <div>
+                    <TextField id="month" label="Month" name="month" error={errors.month?.message}
+                               placeholder="MM"
+                               inputProps={register('month', {
+                                   onChange: (e) => {
+                                       e.target.value = e.target.value.replace(/[^0-9]/g, '')
+                                   },
+                                   onBlur: (e) => {
+                                       const month = parseInt(e.target.value, 10)
+                                       e.target.value = addLeadingZero(month)
+                                   }
+                               })}/>
+                </div>
+                <div>
+                    <TextField id="year" label="Year" name="year" error={errors.year?.message}
+                               placeholder="YYYY"
+                               inputProps={register('year', {
+                                   onChange: (e) => {
+                                       e.target.value = e.target.value.replace(/[^0-9]/g, '')
+                                   }
+                               })}/>
+                </div>
+            </div>
+            <div className={styles.submitHandler}>
+                <div/>
+                <button type="submit">
+                    <SubmitIcon/>
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default App
